1129574b8472425075fcd883ce8c28c0ada1cf62
HEIN KO <teacher.heinko@gmail.com>
2025-08-24 19:36:20 -0700

    Update Node.js code with brilliant message

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 8081;
+const http = require('http');
+const port = 8082;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from Hein’s DevOps App!</h1>');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("Hein Ko, you are brilliant. You are about to be what you would like to be. Congratulations!");
 });
 
-app.listen(port, '0.0.0.0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${port}`);
 });
